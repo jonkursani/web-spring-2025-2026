89eab5d83c07ddfc68a080d24ad802bfa17056f2
Jon Kursani <jonkursani@gmail.com>
2025-10-22 14:01:47 +0200

    add grades and movies pages with user information display

--- 05-thymeleaf-example/src/main/java/dev/jonkursani/thymeleafexample/HomeController.java
+++ 05-thymeleaf-example/src/main/java/dev/jonkursani/thymeleafexample/HomeController.java
@@ -30,22 +30,38 @@ public class HomeController {
         );
         m.addAttribute("products", products);
 
+        User user = new User("John", 20, "john@example.com");
+        m.addAttribute("user", user);
+
+        List<User> users = List.of(
+                new User("John", 20, "john@example.com"),
+                new User("Jane", 22, "jane@example.com"),
+                new User("Bob", 25, "bob@example.com")
+        );
+        m.addAttribute("users", users);
+
         return "home"; // "home" na e percakton ni file me emrin home.html (case sensitive)
     }
 
+    @GetMapping("/movies")
+    public String movies(Model model) {
+        List<String> movies = List.of("Movie 1", "Movie 2", "Movie 3");
+        model.addAttribute("movies", movies);
+        return "movies"; // movies.html
+    }
 
+    @GetMapping("/grades")
+    public String grades(Model model) {
+        Map<String, Integer> grades = Map.of(
+                "Math", 10,
+                "Web Development", 8,
+                "English", 7
+        );
 
+        model.addAttribute("grades", grades);
 
-
-
-
-
-
-
-
-
-
-
+        return "grades";
+    }
 
 
 
